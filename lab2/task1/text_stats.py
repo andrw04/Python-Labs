0ba0e6588fa510ec090c4b437bdfa4fa682433e0
Andrey <sivyandrey2021@gmail.com>
2023-03-28 00:23:14 +0300

    Fixed ReGex

--- lab2/task1/text_stats.py
+++ lab2/task1/text_stats.py
@@ -26,7 +26,7 @@ def get_all_words(text):
 
 
 def get_all_sentences(text):
-    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', text)
 
     return sentences
 
@@ -60,7 +60,6 @@ def average_word_length(text):
 def average_sentence_length(text):
     sentences = get_all_sentences(text)
     average = 0
-
     for sentence in sentences:
         words = get_all_words(sentence)
         for word in words:
@@ -85,7 +84,7 @@ def top_repeated(text,k=10, n=4):
     
     sorted_word = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
-    top_k_repeated = {}
+    top_k_repeated = dict()
 
     for key, value in sorted_word:
         if k == 0:
@@ -106,10 +105,15 @@ def main():
     non_declarative = amount_non_declarative(text)
     av_w_len = average_word_length(text)
     av_sent_len = average_sentence_length(text)
+    k = int(input('Enter k: '))
+    n = int(input('Enter n: '))
+    most_repeated = top_repeated(text, k, n)
     print(f"Amount of sentences: {sent_amount}")
     print(f"Amount of non-declarative sentences: {non_declarative}")
     print(f"Average word length: {av_w_len}")
     print(f"Average senteces length: {av_sent_len}")
+    print('Most repeated:')
+    print(most_repeated)
 
 
 if __name__ == '__main__':
