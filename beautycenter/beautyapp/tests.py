b58ba1976753835e84976417b6edc444b876ce6d
Andrey <sivyandrey2021@gmail.com>
2023-06-07 03:08:47 +0300

    Completed lab

--- beautycenter/beautyapp/tests.py
+++ beautycenter/beautyapp/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+
+def test_home_page():
+    url = 'http://127.0.0.1:8080/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_about_page():
+    url = 'http://127.0.0.1:8080/services/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_film_page_1():
+    url = 'http://127.0.0.1:8080/services/add/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_film_page2():
+    url = 'http://127.0.0.1:8080/categories/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_film_page3():
+    url = 'http://127.0.0.1:8080/categories/add/'
+    response = requests.get(url)
+    assert response.status_code == 404
+
+
+def test_genre_page1():
+    url = 'http://127.0.0.1:8080/clients/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_genre_page2():
+    url = 'http://127.0.0.1:8080/doctors/'
+    response = requests.get(url)
+    assert response.status_code == 200
+
+
+def test_genre_page3():
+    url = 'http://127.0.0.1:8080/doctors/add/'
+    response = requests.get(url)
+    assert response.status_code == 404
